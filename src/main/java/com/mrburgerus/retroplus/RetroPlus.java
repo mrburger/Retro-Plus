e6958612cfdeca2dcad68c13788694606d332280
mrburgerUS <nicklekson@gmail.com>
2021-05-22 10:49:43 -0500

    Initial Commit, trying to rewrite the mod.

---
+++ src/main/java/com/mrburgerus/retroplus/RetroPlus.java
@@ -0,0 +1,16 @@
+package com.mrburgerus.retroplus;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("retroplus")
+public class RetroPlus
+{
+    private static final Logger LOGGER = LogManager.getLogger(); // Logger
+
+    public RetroPlus()
+    {
+
+    }
+}
